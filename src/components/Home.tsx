07ea9e32554ac01db233916aa012406fca62aa06
rjoohappeh <tyler153@gmail.com>
2023-09-17 13:52:05 -0600

    Added react-router

---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+import { Search } from "./Search";
+
+export function Home() {
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            <Search/>
+        </div>
+    );
+}
