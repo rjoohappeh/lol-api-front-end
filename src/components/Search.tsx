419ac395aba90d01e738bf32a870f72d66fcaef3
rjoohappeh <tyler153@gmail.com>
2023-09-17 19:09:58 -0600

    Updated error handling for search bar to handle 404 responses from the server'

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,12 +2,10 @@ import { useRef, useState } from "react";
 import { getRankedStatsBySummonerName } from "../api/user-account/user-account-api";
 import { Button } from "./shared/Button";
 import { useNavigate } from "react-router-dom";
-import useFocus from "../hooks/useFocus";
 
 export function Search() {
     const [accountName, setAccountName] = useState<string>('');
     const [error, setError] = useState<string>('');
-    const [focusRef, setFocus] = useFocus<HTMLInputElement>();
     const inputRef = useRef<HTMLInputElement>(null);
 
     const navigate = useNavigate();
@@ -20,7 +18,11 @@ export function Search() {
                 navigate(`/summoner/${accountName}`, { state: data });
             })
             .catch((err) => {
-                setError(err);
+                if (err.message) {
+                    setError('No account found with that summoner name');
+                } else {
+                    setError(err);
+                }
                 setAccountName('');
                 inputRef?.current?.focus();
             });
@@ -28,6 +30,7 @@ export function Search() {
     
     return (
         <form className="search-bar-container w-2/5 justify-center" onSubmit={onSearchSubmit}>
+            {error && <p className="text-red-500"><small>{error}</small></p>}
             <input 
                 className={`transition search w-4/5 p-3 border-solid border-2 focus:outline-none ${error ? 'border-red-500' : 'border-black focus-visible:border-blue-700'} rounded-lg`} 
                 type="text" 
