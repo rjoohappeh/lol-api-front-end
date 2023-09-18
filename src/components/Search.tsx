206b9f7b27b6e62d8cb4ccce65f064d3543cb09d
rjoohappeh <tyler153@gmail.com>
2023-09-17 18:57:53 -0600

    Add autofocus and refocus on submit functionality to the input

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { getRankedStatsBySummonerName } from "../api/user-account/user-account-api";
 import { Button } from "./shared/Button";
 import { useNavigate } from "react-router-dom";
+import useFocus from "../hooks/useFocus";
 
 export function Search() {
     const [accountName, setAccountName] = useState<string>('');
-    const [ error, setError ] = useState<string>('');
+    const [error, setError] = useState<string>('');
+    const [focusRef, setFocus] = useFocus<HTMLInputElement>();
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const navigate = useNavigate();
 
@@ -18,16 +21,21 @@ export function Search() {
             })
             .catch((err) => {
                 setError(err);
+                setAccountName('');
+                inputRef?.current?.focus();
             });
     }
     
     return (
         <form className="search-bar-container w-2/5 justify-center" onSubmit={onSearchSubmit}>
             <input 
-                className="transition search w-4/5 p-3 border-solid border-2 border-black focus-visible:border-blue-700 focus:outline-none rounded-lg" 
+                className={`transition search w-4/5 p-3 border-solid border-2 focus:outline-none ${error ? 'border-red-500' : 'border-black focus-visible:border-blue-700'} rounded-lg`} 
                 type="text" 
                 placeholder="Search for account by name" 
                 onChange={(event) => setAccountName(event.target.value)}
+                value={accountName}
+                ref={inputRef}
+                autoFocus
             />
             <Button type={"submit"} disabled={accountName.length === 0} text={"Search"}/>
         </form>
