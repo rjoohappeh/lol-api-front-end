24364180a1eb100d84dd64b790b5d12b2680d26a
rjoohappeh <tyler153@gmail.com>
2023-09-19 16:48:07 -0600

    Updated navbar to have reusable classList variable and different background for navlinks when the navlink is active

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,19 @@
 import { NavLink } from "react-router-dom";
 
 export function Navbar() {
+    const linkClassList = 'transition p-3 hover:text-blue-700 hover:cursor-pointer hover:bg-gray-300 rounded-b-xl h-full inline-block';
     return (
         <nav className="navbar h-12 w-4/5 bg-gray-200 flex m-auto text-black rounded-b-xl">
             <div>
                 <ul>
-                    <NavLink to="/" className={({ isActive }) => {
-                            const classList = 'p-3 hover:text-blue-700 hover:cursor-pointer hover:bg-gray-300 rounded-b-xl h-full inline-block';
-                            return isActive ? `${classList}` : classList;
-                        }
-                    }>
-                        Summoner Name
-                    </NavLink>
+                    <li>
+                        <NavLink to="/" className={({ isActive }) => {
+                                return isActive ? `${linkClassList} bg-gray-300` : linkClassList;
+                            }
+                        }>
+                            Summoner Name
+                        </NavLink>  
+                    </li>
                 </ul>
             </div>
         </nav>
